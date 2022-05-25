5c9a34f0ebebb8d654fcbe20f5e1256e0b53bdbf
DarrylS <darrylsantosa9@gmail.com>
2022-05-26 02:42:43 +0700

    Commit pertama

---
+++ vue-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <CostDetail></CostDetail>
+    <b-button variant="danger">Button</b-button>
+  </div>
+</template>
+
+<script>
+import CostDetail from './components/CostDetail.vue'
+export default {
+  name: 'App',
+  components: {
+    CostDetail
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  border: solid;
+}
+</style>
---
+++ vue-app/src/components/CostDetail.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+<h1>Ini Cost Detail</h1>
+<b-button variant="success">Button</b-button>
+</div>
+</template>
+<script>
+export default {
+  name: 'CostDetail'
+}
+</script>
+<style>
+</style>
